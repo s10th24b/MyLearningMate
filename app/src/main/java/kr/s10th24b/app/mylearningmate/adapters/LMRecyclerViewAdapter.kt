117f40f28b4e8c1038c16132093d361fb6b296a1
s10th24b <gywn6209@gmail.com>
2021-06-07 21:31:02 +0900

    change Log.d to logD

--- app/src/main/java/kr/s10th24b/app/mylearningmate/adapters/LMRecyclerViewAdapter.kt
+++ app/src/main/java/kr/s10th24b/app/mylearningmate/adapters/LMRecyclerViewAdapter.kt
@@ -10,17 +10,10 @@ import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.jakewharton.rxbinding4.view.clicks
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.subjects.PublishSubject
 import kr.s10th24b.app.mylearningmate.R
 import kr.s10th24b.app.mylearningmate.databinding.LearningMateCardViewBinding
 import kr.s10th24b.app.mylearningmate.model.Task
-import splitties.coroutines.repeatWhileActive
-import splitties.toast.toast
-import java.util.concurrent.TimeUnit
-import javax.inject.Singleton
+import kr.s10th24b.app.mylearningmate.utilities.DevTool.logD
 
 class LMRecyclerViewAdapter :
     ListAdapter<Task, LMRecyclerViewAdapter.LMRecyclerViewHolder>(TaskDiffUtilCallback()) {
@@ -46,7 +39,7 @@ class LMRecyclerViewAdapter :
         // remove button
         holder.binding.clearButton.setOnClickListener {
             holder.binding.clearButton.isClickable = false
-            Log.d("KHJ", "currentList[$position]")
+            logD("currentList[$position]")
             // 삽질 엄청 했다. recyclerView에서 Item을 없앨 때는! 꼭! adapterPosition을 이용하자!
 //            removeButtonObservable.onNext(getItem(position).id)
 //            removeButtonObservable.onNext(getItem(holder.adapterPosition).id)
@@ -54,14 +47,14 @@ class LMRecyclerViewAdapter :
         }
         holder.binding.lmcardView.setOnCreateContextMenuListener { menu, view, menuInfo ->
             val inflater = MenuInflater(view.context)
-            Log.d("KHJ", "view: $view")
+            logD("view: $view")
             listener.onMenuOpened(getItem(holder.adapterPosition))
             inflater.inflate(R.menu.lm_card_context_menu, menu)
         }
     }
 
     override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
-        Log.d("KHJ", "onDetachedFromRecyclerView")
+        logD("onDetachedFromRecyclerView")
         super.onDetachedFromRecyclerView(recyclerView)
     }
 
